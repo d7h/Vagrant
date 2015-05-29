e8d8980adea9890750ec2a58d7aa6e1ea917b5bd
Michael Schnider <michael.schnider.ms@gmail.com>
2015-05-29 12:46:19 +0200

    init

--- puppet/modules/postconfig/manifests/init.pp
+++ puppet/modules/postconfig/manifests/init.pp
@@ -16,13 +16,13 @@ class postconfig {
   #Wegen einem Bug in docker-compose im Moment hier untergebracht und nicht wie die anderen Container im Vagrantfile direkt.
 
   exec { "start_etcd_Container" :    
-    command => "sudo docker run -d -p 4001:4001 -v /usr/share/ca-certificates/:/etc/ssl/certs quay.io/coreos/etcd:v2.0.11 -advertise-client-urls http://192.168.42.77:4001 -listen-client-urls http://0.0.0.0:4001",
+    command => "sudo docker run -d -name etcd -p 4001:4001 -v /usr/share/ca-certificates/:/etc/ssl/certs quay.io/coreos/etcd:v2.0.11 -advertise-client-urls http://192.168.42.77:4001 -listen-client-urls http://0.0.0.0:4001",
     cwd => "/",
     path => "/usr/local/bin:/bin:/usr/bin",
   }
 
   exec { "start_dockerui_Container" :    
-    command => "sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/docker.sock dockerui/dockerui -e /docker.sock",
+    command => "sudo docker run -d -name dockerui -p 9000:9000 -v /var/run/docker.sock:/docker.sock dockerui/dockerui -e /docker.sock",
     cwd => "/",
     path => "/usr/local/bin:/bin:/usr/bin",
   }
